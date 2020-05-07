b3f5e76886b114c466c3254a1605d2a7d6eb0126
AT <35488642+de-cgs-online-atappe@users.noreply.github.com>
2020-05-07 19:58:05 +0200

    append found prototype (relative) URL as anchor

--- stub.js
+++ stub.js
@@ -2,7 +2,7 @@ var links = document.getElementsByTagName("a");
 for (var i=0; i<links.length; i++) {
   var link = links[i].attributes["href"].value;
   if (link.match(/^\/.*\/prototyp\.js$/)) {
-    alert(link);
-	break;
+    location.href = "#" + link;
+    break;
   }
 }
