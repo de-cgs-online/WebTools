fb81cf92c205e62d516c09dccdc4c2bd4b618ff2
AT <35488642+de-cgs-online-atappe@users.noreply.github.com>
2020-05-07 20:02:20 +0200

    Replace page's body with stub.html script

--- stub.js
+++ stub.js
@@ -6,3 +6,28 @@ for (var i=0; i<links.length; i++) {
     break;
   }
 }
+
+document.body.innerHTML = "  <script type=\"text/javascript\">\n" +
+  "    if (location.hash && location.hash.length > 1) {\n" +
+  "      function getLocationHashContents() {\n" +
+  "        return location.hash.substr(1);\n" +
+  "      }\n" +
+  "      function getPrefixPath(url) {\n" +
+  "        return (url.match(/^.*\//) || [\"\"])[0];\n" +
+  "      }\n" +
+  "      function loadJavaScript(url, charset, onloadFunc) {\n" +
+  "        var extScript = document.createElement(\"script\");\n" +
+  "        extScript.type = \"text/javascript\";\n" +
+  "        extScript.charset = charset || \"utf-8\";\n" +
+  "        if (onloadFunc) {\n" +
+  "          extScript.onload = onloadFunc;\n" +
+  "        }\n" +
+  "        extScript.src = url;\n" +
+  "        document.body.appendChild(extScript);\n" +
+  "      }\n" +
+  "      function loadRelativeJavaScript(name, charset, onloadFunc) {\n" +
+  "        loadJavaScript(getPrefixPath(getLocationHashContents()) + name, charset, onloadFunc);\n" +
+  "      }\n" +
+  "      loadJavaScript(getLocationHashContents());\n" +
+  "    }\n" +
+  "  </script>\n";
